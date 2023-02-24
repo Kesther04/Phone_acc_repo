eca63ce2a676295215630c2be4d229ef4e9dab0d
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2023-02-24 15:51:56 +0100

    changes to account form.js

--- js/account_form.js
+++ js/account_form.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
      var form = $("#acc-form");
 
+     $("#fname").keyup(function () {
+        var fna = $("#fname").val();
+        var lna = $("#lname").val();
+        var b = fna+' '+lna;
+        $("#disname").val(b);
+    });
+
     $("#lname").keyup(function () {
         var fna = $("#fname").val();
         var lna = $("#lname").val();
         var b = fna+' '+lna;
         $("#disname").val(b);
-    })
+    });
+
+    
 
     
 })
